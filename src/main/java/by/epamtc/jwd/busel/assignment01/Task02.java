ee19deb6d8f4199a8f1689baabed48960af78ecd
Siarhei Busiel <sju.busel@gmail.com>
2020-06-07 23:09:31 +0300

    Simplify Tasks 02 of Assignment 01 according to trainer's remarks

--- src/main/java/by/epamtc/jwd/busel/assignment01/Task02.java
+++ src/main/java/by/epamtc/jwd/busel/assignment01/Task02.java
@@ -1,65 +1,81 @@
 package by.epamtc.jwd.busel.assignment01;
 
+import by.epamtc.jwd.busel.assignment01.entity.NonLeapYearDate;
+
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Task02 {
-    private static final String DAY_PATTERN = "[0-9]+";
-    private static Scanner scanner = new Scanner(System.in);
-    private static int[] daysPerMonth = new int[]{31, 28, 31, 30, 31, 30, 31, 31,
-            30, 31, 30, 31};
+    private static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
 
     private static int getValidOrdinalDayNumberFromConsole() {
-        String input = scanner.nextLine();
-        if (isInteger(input)) {
-            int dayNumber = Integer.parseInt(input);
-            return (isValid(dayNumber))
-                    ? dayNumber
-                    : getValidOrdinalDayNumberFromConsole();
+        if (scanner.hasNextInt()) {
+            int dayNumber = scanner.nextInt();
+            if (isValid(dayNumber)) {
+                return dayNumber;
+            } else {
+                System.out.printf("There is no day \"%d\"%n", dayNumber);
+            }
         } else {
-            informAboutInvalidScannerInput();
-            return getValidOrdinalDayNumberFromConsole();
+            informAboutInvalidScannerInput(scanner.next());
         }
-    }
-
-    private static boolean isInteger(String input) {
-        return Pattern.matches(DAY_PATTERN, input);
+        return getValidOrdinalDayNumberFromConsole();
     }
 
     private static boolean isValid(int dayNumber) {
         return (dayNumber > 0) && (dayNumber <= 365);
     }
 
-    private static void informAboutInvalidScannerInput() {
-        System.out.println("You have entered an invalid token,");
-        System.out.println("because it can not be translated");
-        System.out.println("into a valid int value!\n");
+    private static void informAboutInvalidScannerInput(String input) {
+        System.out.printf("You have entered an invalid token"
+                + " \"%s\", because it can not be translated into"
+                + " a valid int value!\n", input);
     }
 
-    private static int[] calculateDateAndMonthBy(int ordinalDayNumber) {
-        int day = 0;
+    private static NonLeapYearDate getDateBy(int ordinalDayNumber) {
+        int day = ordinalDayNumber;
         int month = 0;
-        for (int i = 0; i < daysPerMonth.length; i++) {
-            if (ordinalDayNumber > daysPerMonth[i]) {
-                ordinalDayNumber = ordinalDayNumber - daysPerMonth[i];
-            } else {
-                month = ++i;
-                day = ordinalDayNumber;
-                break;
+        int delta = 0;
+        boolean canQuit = false;
+        while (!canQuit) {
+            day = day - delta;
+            month++;
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    delta = 31;
+                    canQuit = ((day - delta) <= 0);
+                    break;
+                case 2:
+                    delta = 28;
+                    canQuit = ((day - delta) <= 0);
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    delta = 30;
+                    canQuit = ((day - delta) <= 0);
+                    break;
             }
         }
-        return new int[]{day, month};
+        return new NonLeapYearDate(day, month);
     }
 
     public static void main(String[] args) {
-        System.out.println("Please, input an ordinal number");
-        System.out.println("of any day of a common (non-leap) year");
+        System.out.println("Please, input an ordinal number of any day of"
+                + " a common (non-leap) year");
         int ordinalDayNumber = getValidOrdinalDayNumberFromConsole();
 
-        int[] dateAndMonth = calculateDateAndMonthBy(ordinalDayNumber);
+        NonLeapYearDate date = getDateBy(ordinalDayNumber);
         System.out.printf("Day \"%s\" and Month \"%s\" conform to",
-                dateAndMonth[0], dateAndMonth[1]);
-        System.out.printf(" an ordinal day number \"%s\"%n",
+                date.getDayNumber(), date.getMonthNumber());
+        System.out.printf(" an ordinal day number \"%s\".%n",
                 ordinalDayNumber);
+        scanner.close();
     }
 }
