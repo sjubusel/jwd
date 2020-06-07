8c33cb0cc21bfac51c0ad98c843917351e691f66
Siarhei Busiel <sju.busel@gmail.com>
2020-06-07 23:12:51 +0300

    Simplify Tasks 04 of Assignment 01 according to trainer's remarks

--- src/main/java/by/epamtc/jwd/busel/assignment01/Task04.java
+++ src/main/java/by/epamtc/jwd/busel/assignment01/Task04.java
@@ -1,57 +1,43 @@
 package by.epamtc.jwd.busel.assignment01;
 
+import java.util.Locale;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Task04 {
-    private static final String SCALAR = "(-?[0-9]+)([.][0-9Ee]+)?";
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in).useDelimiter("\n")
+            .useLocale(Locale.US);
 
-    private static double getValueUnlessItIsDouble() {
-        System.out.println("Please, input any real number");
-        String input = scanner.nextLine().trim();
-        if (isDouble(input)) {
-            return Double.parseDouble(input);
+    private static double getDoubleFromConsole() {
+        if (scanner.hasNextDouble()) {
+            return scanner.nextDouble();
         } else {
-            informAboutInvalidScannerInput();
-            return getValueUnlessItIsDouble();
+            informAboutInvalidScannerInput(scanner.next());
+            return getDoubleFromConsole();
         }
     }
 
-    private static boolean isDouble(String input) {
-        return Pattern.matches(SCALAR, input);
-    }
-
-    private static void informAboutInvalidScannerInput() {
-        System.out.println("You have entered an invalid token,");
-        System.out.println("because it can not be translated");
-        System.out.println("into a valid double value!\n");
+    private static void informAboutInvalidScannerInput(String input) {
+        System.out.printf("You have entered an invalid token"
+                + " \"%s\", because it can not be translated into"
+                + " a valid int value!\n", input);
     }
 
     private static double computeFunction(double x) {
         if (x <= 13) {
-            return computeFunctionIfTrue(x);
+            return ((-1.0) * Math.pow(x, 3.0) + 9.0);
         } else {
-            return computeFunctionIfFalse(x);
+            return ((-1.0) * (3.0 / (x + 1.0)));
         }
     }
 
-    private static double computeFunctionIfTrue(double x) {
-        return (-1.0 * Math.pow(x, 3.0) + 9.0);
-    }
+    public static void main(String[] args) {
+        System.out.println("Please, input any real number");
+        double x = getDoubleFromConsole();
+        double y = computeFunction(x);
 
-    private static double computeFunctionIfFalse(double x) {
-        return -1.0 * (3.0 / (x + 1.0));
-    }
-
-    private static void printValues(double x, double y) {
         System.out.printf("The value of function \"y\" equals to %g%n", y);
         System.out.printf("if the value of Variable \"x\" is %g%n", x);
-    }
 
-    public static void main(String[] args) {
-        double x = getValueUnlessItIsDouble();
-        double y = computeFunction(x);
-        printValues(x, y);
+        scanner.close();
     }
 }
