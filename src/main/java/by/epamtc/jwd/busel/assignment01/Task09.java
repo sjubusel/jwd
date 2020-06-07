34a778de84dda50561daf6f357562ee4fc3cf0f3
Siarhei Busiel <sju.busel@gmail.com>
2020-06-07 23:31:15 +0300

    Simplify Tasks 09 of Assignment 01 according to trainer's remarks

--- src/main/java/by/epamtc/jwd/busel/assignment01/Task09.java
+++ src/main/java/by/epamtc/jwd/busel/assignment01/Task09.java
@@ -4,34 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Task09 {
-    private static double computeFunction() {
-        List<Double> elementsToMultiply = getElementsToMultiply();
-        return multiplyElements(elementsToMultiply);
-    }
-    
-    private static List<Double> getElementsToMultiply() {
-        List<Double> elements = new ArrayList<>(9);
-        double a1 = 1;
-        double sum;
-        for (int aN = 2; aN <= 10; aN++) {
-            sum = aN / 2.0 * (a1 + aN);
-            elements.add(sum);
-        }
-        return elements;
-    }
-    
-    private static double multiplyElements(List<Double> elementsToMultiply) {
-        double product = 1.0;
-        for (double element : elementsToMultiply) {
-            product = product * element;
+    private static long computeFunction() {
+        long sum = 1;
+        long product = 1;
+        for (int i = 2; i <= 10; i++) {
+            sum += i;
+            product *= sum;
         }
         return product;
     }
-    
+
     public static void main(String[] args) {
         System.out.println("Currently computing the following mathematical" +
                 " expression:\n(1+2) * (1+2+3) * ... * (1+2+...+10).");
-        double result = computeFunction();
-        System.out.printf("The result of computation is %f", result);
+        long result = computeFunction();
+        System.out.printf("The result of computation is %d.", result);
     }
 }
