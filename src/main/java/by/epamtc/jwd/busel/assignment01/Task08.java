eef3705a85719ad5c4503c431576287df4b4574c
Siarhei Busiel <sju.busel@gmail.com>
2020-06-07 23:29:50 +0300

    Simplify Tasks 08 of Assignment 01 according to trainer's remarks

--- src/main/java/by/epamtc/jwd/busel/assignment01/Task08.java
+++ src/main/java/by/epamtc/jwd/busel/assignment01/Task08.java
@@ -1,53 +1,48 @@
 package by.epamtc.jwd.busel.assignment01;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Task08 {
-    private static final String SCALAR = "(-?[0-9]+)([.][0-9Ee]+)?";
-    private static final double CONST = Math.PI;
-    private static Scanner scanner = new Scanner(System.in);
-    
+    private static Scanner scanner = new Scanner(System.in).useDelimiter("\n")
+            .useLocale(Locale.US);
+
     private static double getDoubleFromConsole() {
-        String input = scanner.nextLine().trim();
-        if (isDouble(input)) {
-            return Double.parseDouble(input);
+        if (scanner.hasNextDouble()) {
+            return scanner.nextDouble();
         } else {
-            informAboutInvalidScannerInput();
+            informAboutInvalidScannerInput(scanner.next());
             return getDoubleFromConsole();
         }
     }
-    
-    private static boolean isDouble(String input) {
-        return Pattern.matches(SCALAR, input);
+
+    private static void informAboutInvalidScannerInput(String input) {
+        System.out.printf("You have entered an invalid token \"%s\","
+                + " because it can not be translated into a valid"
+                + " double value!%n", input);
     }
-    
-    private static void informAboutInvalidScannerInput() {
-        System.out.println("You have entered an invalid token,"
-                + "because it can not be translated into a valid"
-                + " double value!");
-    }
-    
+
+    //TODO create javaDoc
     private static Map<Double, Double> getFunctionValues(double openingPoint,
             double closingPoint, double step) {
         Map<Double, Double> pairsXY = new LinkedHashMap<>();
-        int accuracyAfterPoint = getAccuracyAfterPoint(openingPoint, step);
+        int accuracy = getAccuracyAfterPoint(openingPoint, step);
         for (double x = openingPoint; x <= closingPoint; ) {
             double y = computeFunction(x);
             pairsXY.putIfAbsent(x, y);
-            x = getAccurateValue(x + step, accuracyAfterPoint);
+            x = getAccurateValue(x + step, accuracy);
         }
         return pairsXY;
     }
-    
+
     private static int getAccuracyAfterPoint(double openingPoint, double step) {
         int digitsNumber = getDigitsNumberAfterPoint(openingPoint);
         int digitsNumber2 = getDigitsNumberAfterPoint(step);
         return Math.max(digitsNumber, digitsNumber2);
     }
-    
+
     private static double getAccurateValue(double realNumber, int accuracy) {
         int digitsNumberAfterPoint = getDigitsNumberAfterPoint(realNumber);
         if (digitsNumberAfterPoint > accuracy) {
@@ -55,41 +50,34 @@ public class Task08 {
         }
         return realNumber;
     }
-    
-    private static double trimToGetAccurateValue(double realNumber,
-            int accuracy) {
-        double tempNumber = realNumber * Math.pow(10, accuracy);
-        tempNumber = Math.round(tempNumber);
-        realNumber = tempNumber * Math.pow(10, (-1) * accuracy);
-        return realNumber;
-    }
-    
+
     private static int getDigitsNumberAfterPoint(double realNumber) {
         String number = Double.toString(realNumber);
         String[] parts = number.split("[.]");
         return parts[1].length();
     }
-    
+
+    private static double trimToGetAccurateValue(double realNumber,
+            int accuracy) {
+        double tempNumber = realNumber * Math.pow(10.0, accuracy);
+        tempNumber = Math.round(tempNumber);
+        realNumber = tempNumber * Math.pow(10, (-1) * accuracy);
+        return realNumber;
+    }
+
     private static double computeFunction(double x) {
-        return (x != 15) ? computeFunctionIfNotEquals(x)
-                         : computeFunctionIfEquals(x);
+        double CONST = Math.PI;
+        return (x != 15) ? ((x - CONST) + 6)
+                         : ((x + CONST) * 2);
     }
-    
-    private static double computeFunctionIfNotEquals(double x) {
-        return ((x - CONST) + 6);
-    }
-    
-    private static double computeFunctionIfEquals(double x) {
-        return ((x + CONST) * 2);
-    }
-    
+
     private static void print(Map<Double, Double> pairs) {
         for (Map.Entry<Double, Double> pair : pairs.entrySet()) {
             System.out.printf("If x = %f, y = %f.\n", pair.getKey(),
                     pair.getValue());
         }
     }
-    
+
     public static void main(String[] args) {
         System.out.println("Please, insert an opening point \"a\" of"
                 + " a line segment.");
@@ -99,9 +87,10 @@ public class Task08 {
         double closingPoint = getDoubleFromConsole();
         System.out.println("Please, insert a function step \"h\".");
         double step = getDoubleFromConsole();
-        
+
         Map<Double, Double> pairs = getFunctionValues(openingPoint,
                 closingPoint, step);
         print(pairs);
+        scanner.close();
     }
 }
