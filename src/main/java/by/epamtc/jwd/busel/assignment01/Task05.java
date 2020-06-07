c7803cf0cfeb3c4b100acee42390f0f4efc68f2e
Siarhei Busiel <sju.busel@gmail.com>
2020-06-07 23:26:46 +0300

    Simplify Tasks 05 of Assignment 01 according to trainer's remarks

--- src/main/java/by/epamtc/jwd/busel/assignment01/Task05.java
+++ src/main/java/by/epamtc/jwd/busel/assignment01/Task05.java
@@ -1,84 +1,79 @@
 package by.epamtc.jwd.busel.assignment01;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Task05 {
-    private static final String INTEGER_PATTERN = "[0-9]+";
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
 
     private static int getThreeFiguresNumberFromConsole() {
-        System.out.println("Please, insert a three figures number.");
-        String input = scanner.nextLine();
-        if (isInteger(input)) {
-            int number = Integer.parseInt(input);
-            return (isValid(number))
-                    ? number
-                    : getThreeFiguresNumberFromConsole();
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            if (isThreeDigit(number)) {
+                return number;
+            } else {
+                System.out.printf("Invalid input \"%d\"", number);
+            }
         } else {
-            informAboutInvalidScannerInput();
-            return getThreeFiguresNumberFromConsole();
+            informAboutInvalidScannerInput(scanner.next());
         }
+        return getThreeFiguresNumberFromConsole();
     }
 
-    private static boolean isInteger(String input) {
-        return Pattern.matches(INTEGER_PATTERN, input);
-    }
-
-    private static boolean isValid(int number) {
+    private static boolean isThreeDigit(int number) {
         return (number >= 100) && (number <= 999);
     }
 
-    private static void informAboutInvalidScannerInput() {
-        System.out.println("You have entered an invalid token,");
-        System.out.println("because it can not be translated");
-        System.out.println("into a valid int value!\n");
+    private static void informAboutInvalidScannerInput(String input) {
+        System.out.printf("You have entered an invalid token \"%s\","
+                + " because it can not be translated into a valid int"
+                + " value!\n", input);
     }
 
-    private static int[] getDigitsInAscendingPriority(int threeFigures) {
-        int unitsDigit = threeFigures % 10;
-        int tensDigit = ((threeFigures % 100) - unitsDigit) / 10;
-        int hundredsDigit = threeFigures / 100;
-        return new int[]{unitsDigit, tensDigit, hundredsDigit};
-    }
-
-    private static int computeAdditionOfDigits(int[] digits) {
+    private static int computeAdditionOfDigits(int number) {
         int sum = 0;
-        for (int digit : digits) {
-            sum += digit;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
         }
         return sum;
     }
 
-    private static int computeMultiplicationOfDigits(int[] digits) {
+    private static int computeMultiplicationOfDigits(int number) {
         int product = 1;
-        for (int digit : digits) {
-            product *= digit;
+        while (number != 0) {
+            product *= number % 10;
+            number /= 10;
         }
         return product;
     }
 
-    private static int computeHundredsAndTensRearrangement(int[] digits) {
-        return digits[1] * 100 + digits[2] * 10 + digits[0];
+    private static int computeHundredsAndTensRearrangement(int number) {
+        int unitsDigit = number % 10;
+        number /= 10;
+        int tensDigit = number % 10;
+        number /= 10;
+        int hundredsDigit = number % 10;
+        return tensDigit * 100 + hundredsDigit * 10 + unitsDigit;
     }
 
-    private static int computeFourFiguresFrom(int[] digits) {
-        return digits[0] * 1000 + digits[2] * 100 + digits[1] * 10
-                + digits[0];
+    private static int computeFourFiguresFrom(int number) {
+        int thousandsDigit = number % 10;
+        return thousandsDigit * 1000 + number;
     }
 
     public static void main(String[] args) {
-        int threeFiguresNumber = getThreeFiguresNumberFromConsole();
-        int[] digits = getDigitsInAscendingPriority(threeFiguresNumber);
+        System.out.println("Please, insert a three figures number.");
+        int number = getThreeFiguresNumberFromConsole();
 
-        int sumOfDigits = computeAdditionOfDigits(digits);
-        int productOfDigits = computeMultiplicationOfDigits(digits);
-        int rearrangedNumber = computeHundredsAndTensRearrangement(digits);
-        int fourFiguresNumber = computeFourFiguresFrom(digits);
+        int sumOfDigits = computeAdditionOfDigits(number);
+        int productOfDigits = computeMultiplicationOfDigits(number);
+        int rearrangedNumber = computeHundredsAndTensRearrangement(number);
+        int fourFiguresNumber = computeFourFiguresFrom(number);
 
         System.out.printf("Sum of digits: %d%n", sumOfDigits);
         System.out.printf("Product of digits: %d%n", productOfDigits);
         System.out.printf("Rearranged input number: %d%n", rearrangedNumber);
         System.out.printf("Four figures number: %d%n", fourFiguresNumber);
+        scanner.close();
     }
 }
