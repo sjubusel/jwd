a99e0bbc14e3e70c39ed245ced48b56d21143d70
Siarhei Busiel <sju.busel@gmail.com>
2020-06-07 22:52:45 +0300

    Simplify Tasks 01 of Assignment 01 according to trainer's remarks

--- src/main/java/by/epamtc/jwd/busel/assignment01/Task01.java
+++ src/main/java/by/epamtc/jwd/busel/assignment01/Task01.java
@@ -1,77 +1,57 @@
 package by.epamtc.jwd.busel.assignment01;
 
+import java.util.Locale;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Task01 {
-    private static final String SCALAR = "(-?[0-9]+)([.][0-9Ee]+)?";
-    private static Scanner scanner = new Scanner(System.in);
-    private static String[] variableName = {"a", "b", "c"};
+    private static Scanner scanner = new Scanner(System.in).useDelimiter("\n")
+            .useLocale(Locale.US);
 
-    private static double getDoubleFromConsoleNamedAs(String variableName) {
-        System.out.printf("Please, insert a value of a \"%s\" variable.%n",
-                variableName);
-        return getValueUnlessItIsDouble(variableName);
-    }
-
-    private static double getValueUnlessItIsDouble(String variableName) {
-        String input = scanner.nextLine().trim();
-        if (isDouble(input)) {
-            return Double.parseDouble(input);
+    private static double getDoubleFromConsole() {
+        if (scanner.hasNextDouble()) {
+            return scanner.nextDouble();
         } else {
-            informAboutInvalidScannerInput();
-            return getDoubleFromConsoleNamedAs(variableName);
+            informAboutInvalidScannerInput(scanner.next());
+            return getDoubleFromConsole();
         }
     }
 
-    private static boolean isDouble(String input) {
-        return Pattern.matches(SCALAR, input);
-    }
-
-    private static void informAboutInvalidScannerInput() {
-        System.out.println("You have entered an invalid token,");
-        System.out.println("because it can not be translated");
-        System.out.println("into a valid double value!\n");
+    private static void informAboutInvalidScannerInput(String input) {
+        System.out.printf("You have entered an invalid token \"%s\","
+                + " because it can not be translated into a valid"
+                + " double value!\n", input);
     }
 
     private static double computeExpression(double a, double b, double c) {
-        reportInputValuesToConsole(a, b, c);
-        double firstExpressionPart = computeFirstExpressionPart(a, b, c);
-        double secondExpressionPart = computeSecondExpressionPart(a, c);
-        double thirdExpressionPart = computeThirdExpressionPart(b);
+        double dividend = b + Math.sqrt(Math.pow(b, 2.0) + 4 * a * c);
+        double divisor = 2 * a;
+        double firstExpressionPart = dividend / divisor;
+        double secondExpressionPart = Math.pow(a, 3.0) * c;
+        double thirdExpressionPart = Math.pow(b, -2.0);
         return firstExpressionPart - secondExpressionPart + thirdExpressionPart;
     }
 
-    private static double computeFirstExpressionPart(double a, double b,
-                                                     double c) {
-        double dividend = b + Math.sqrt(Math.pow(b, 2.0) + 4 * a * c);
-        double divisor = 2 * a;
-        return dividend / divisor;
-    }
-
-    private static double computeSecondExpressionPart(double a, double c) {
-        return Math.pow(a, 3.0) * c;
-    }
-
-    private static double computeThirdExpressionPart(double b) {
-        return Math.pow(b, -2.0);
-    }
-
     private static void reportInputValuesToConsole(double a, double b,
-                                                   double c) {
+            double c) {
         System.out.println("You have entered the following variables:");
-        System.out.printf("- variable \"%s\" is %f%n", variableName[0], a);
-        System.out.printf("- variable \"%s\" is %f%n", variableName[1], b);
-        System.out.printf("- variable \"%s\" is %f%n", variableName[2], c);
+        System.out.printf("- variable \"%s\" is %f%n", "a", a);
+        System.out.printf("- variable \"%s\" is %f%n", "b", b);
+        System.out.printf("- variable \"%s\" is %f%n", "c", c);
     }
 
     public static void main(String[] args) {
-        double a = getDoubleFromConsoleNamedAs(variableName[0]);
-        double b = getDoubleFromConsoleNamedAs(variableName[1]);
-        double c = getDoubleFromConsoleNamedAs(variableName[2]);
+        System.out.println("Please, insert a value of a \"a\" variable.");
+        double a = getDoubleFromConsole();
+        System.out.println("Please, insert a value of a \"b\" variable.");
+        double b = getDoubleFromConsole();
+        System.out.println("Please, insert a value of a \"c\" variable.");
+        double c = getDoubleFromConsole();
+
+        reportInputValuesToConsole(a, b, c);
 
         double expression = computeExpression(a, b, c);
         System.out.printf("The result of computing is%n%g", expression);
+
         scanner.close();
     }
 }
